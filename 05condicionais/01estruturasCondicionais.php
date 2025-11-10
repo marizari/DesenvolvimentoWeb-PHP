b48cba6ef5a7a6d603982e933aba3ccd9a230f75
Maria Eduarda <marizari2006@gmail.com>
2025-11-09 23:17:59 -0300

    60. Condicional simples: comando if e sintaxes alternativas. tivemos contato com o operador  relacional os <>

--- 05condicionais/01estruturasCondicionais.php
+++ 05condicionais/01estruturasCondicionais.php
@@ -1,13 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Condicionais</title>
 </head>
 <body>
-    <h1>Trabalhando com estruturas condicionais</h1>
+    <h1 style="text-align: center;">Trabalhando com estruturas condicionais</h1>
     <hr>
+
+    <h2 style="font-size: 14">Condicional SIMPLES: <code>if (se)</code></h2>
+    <?php
+        $numero = 50;
+        //Estrutura condicional simples
+        if($numero > 10){ //ou :
+            echo "<p>$numero é maior que 10.</p>";
+        } //endif;
+
+        //Com a condição continuando sendo simples podemos:
+        //omitir as chaves {}
+        //pq se trata de apenas uma instrução
+        if($numero > 10) echo "<p>$numero é menor que 10.</p>";
+     
+    ?>
     
 </body>
 </html>

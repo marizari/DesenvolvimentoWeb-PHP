eec5b1beef089d351e87c96de150f5faf68520f9
Maria Eduarda <marizari2006@gmail.com>
2025-11-12 13:42:02 -0300

    Condicional COMPOSTA

--- 05condicionais/01estruturasCondicionais.php
+++ 05condicionais/01estruturasCondicionais.php
@@ -9,30 +9,48 @@
     <h1 style="text-align: center;">Trabalhando com estruturas condicionais</h1>
     <hr>
 
-    <h2 style="font-size: 14">Condicional SIMPLES: <code>if (se)</code></h2>
+    <h2 style="font-size: 14px;">Condicional SIMPLES: <code>if (se)</code></h2>
     <?php
-    /*Operadores RELACIONAIS (de comparação): muito usados em estruturas condicionais
-    ==  Igualdade de valor 
-    === Igualdade de valor e tipo de dado
-    != ou <> (mesma finalidade)  Diferente
-    >   Maior que
-    <   Menor que
-    >=  Maior ou igual a
+    /* Operadores RELACIONAIS (de comparação)
+        ==  Igualdade de valor 
+        === Igualdade de valor e tipo de dado
+        !=  ou <>  Diferente
+        >   Maior que
+        <   Menor que
+        >=  Maior ou igual a
+        <=  Menor ou igual a
+    */
 
+    $numero = 50;
 
+    // Estrutura condicional simples
+    if($numero > 10){
+        echo "<p>$numero é maior que 10.</p>";
+    }
 
-        $numero = 50;
-        //Estrutura condicional simples
-        if($numero > 10){ //ou :
-            echo "<p>$numero é maior que 10.</p>";
-        } //endif;
-
-        //Com a condição continuando sendo simples podemos:
-        //omitir as chaves {}
-        //pq se trata de apenas uma instrução
-        if($numero > 10) echo "<p>$numero é menor que 10.</p>";
-     
+    // Também pode omitir as chaves {} se for só uma linha
+    if($numero < 10) echo "<p>$numero é menor que 10.</p>";
     ?>
+    <hr>
+
+    <h2>Condicional COMPOSTA: <code>if (se) / else (senão)</code></h2>
+    <?php 
+    $produto = "Ultrabook";
+    $qtdEmEstoque = 10; // o que temos à disposição
+    $qtdCritica = 5;    // mínimo necessário em estoque
+
+    echo "<h3>$produto</h3>";
+    echo "<p><b>Quantidade em estoque:</b> $qtdEmEstoque</p>";
+
+    if ($qtdEmEstoque < $qtdCritica){
+        echo "<p style='color:red;'>É necessário comprar/repor!</p>";
+    } else {
+        echo "<p style='color:green;'>Estoque normal.</p>";
+    }
+    ?>
+    <hr>
+
+   
     
 </body>
 </html>
